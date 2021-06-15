e90cd4ae17104736b5ef98de5bb04e68ecaa26e9
박현우(Park Hyun Woo) <46596758+gusdn3477@users.noreply.github.com>
2021-06-15 18:33:33 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+npm install 
+npm start
+# TMAX-CLOUD-EDUCATION
---
+++ src/components/elements/widgets/product/productTop/ProDetRgtBottom.js
@@ -0,0 +1,31 @@
+import { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function ProDetRgtBottom(){
+
+    return(
+        <Fragment>
+            <div className="pro-details-meta">
+                <span>Categories :</span>
+                <ul>
+                    <li><a href="/shop-grid-standard">fashion</a></li>
+                    
+                </ul>
+            </div>
+            <div className="pro-details-meta">
+                <span>Tags :</span>
+                <ul>
+                    <li><a href="/shop-grid-standard">fashion</a></li>
+                    
+                </ul>
+            </div>
+            <div className="pro-details-social">
+                <ul>
+                    <li><Link to="//facebook.com"><i className="fab fa-facebook-f"></i></Link></li>
+                    <li><Link to="//naver.com"><i className="fab fa-line"></i></Link></li>
+                    <li><Link to="//twitter.com"><i className="fab fa-twitter"></i></Link></li>
+                </ul>
+            </div>
+        </Fragment>
+    )
+}
